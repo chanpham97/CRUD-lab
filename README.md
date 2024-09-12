6b6141d16692520477cc8f0f7903c3d14bd53d7b
Chan Pham <iamchanpham@gmail.com>
2024-09-12 19:32:52 +0000

    Lab template

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # CRUD-lab
+Complete the lab based on the instructions in the comments. Make sure to commit at the appropriate points and push to submit the assignment.
+
+## Rubric
+Tasks (10 points)
+Git (2 points)
+- commit messages at the right point with appropriate messages
+- submitting correctly by pushing
+Extra Credit (2 points)
---
+++ index.js
@@ -0,0 +1,78 @@
+// ARRAY SECTION
+// Task 1 [1.5]
+// Create an array called truths and store 3 true facts about yourself
+
+// Create a second array called lies and store 3 fake facts about yourself
+
+// Task 2 [1.5]
+// Console log the 1st element of the truths array (element is not same as index!)
+
+// Change the value of the last element in the truths to another fact
+
+// Console log the last element to check that it's changed
+
+// Task 3 [1.5]
+// Delete the first value from the lies array
+
+// Add a new lie to the end of the lies array
+
+// Console log the lies list to confirm the changes
+
+// Task 4 [1.4]
+// Prompt the user to enter 0, 1, or 2 and save it in a variable called index
+
+// Cast index to be a number
+
+// Task 5 [1.6]
+// Write an if/else statement that checks:
+// - if randomNumber is less than 0.5, console log the element at index from the truths array
+// - else, console log the element at index from the lies array
+let randomNumber = Math.random()
+
+// Task 6 [1.6]
+// Prompt the user to guess if it's true or false
+
+// Use a similar if/else statement as above to print the correct answer
+
+// Commit your changes with an appropriate message.
+// ----------------------------------
+// OBJECTS SECTION
+// Task 7 [1.5]
+// Create a variable called user1 with the keys username (any string), password (any string), friendCount (any number), and isOnline (false).
+
+// Console log the profile's username
+
+// Task 8 [1.5]
+// Make the friend count go up by 1
+
+// Console log the profile's friendCount to check if the value updated
+
+// Task 9 [1.6]
+// Prompt the user to enter their password
+
+// If the user's password is correct, console log "Welcome " + username and set their isOnline to true
+
+// Task 10 [1.5]
+// Add a status key to the user1 object and set it to null
+
+// Prompt the user to enter a status and save it in a variable
+
+// Set the status key to be the user's input
+
+// Commit your changes with an appropriate message.
+// Push the changes if you are ready to submit.
+// ----------------------------------
+// EXTRA CREDIT
+// Add a truths key to the user1 object and set it equal to an empty array
+
+// Do the same for the lies key
+
+// Ask the user for a fact about them and add it to the user1 truths key (not the array above!)
+
+// Ask the user for a fake fact about them and add it to the user1 truths key (not the array above!)
+
+// Set up a similar guessing game as in the array section. 
+// Note, you can skip the prompt about picking 0-2 since you only have 1 truth/lie.
+
+
+
